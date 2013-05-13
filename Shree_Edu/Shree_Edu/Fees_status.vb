b9e8611330766e40697901bec37c2a2880007ee4
Ankit-Kulkarni <ankit.kul1890@gmail.com>
2013-05-13 14:24:10 +0530

    Added a menu named add and a sub item name discount. Click on discount adn it will take oyu to disount dialog. Need to add Functionality

--- Shree_Edu/Shree_Edu/Fees_status.vb
+++ Shree_Edu/Shree_Edu/Fees_status.vb
@@ -3,22 +3,22 @@
 Public Class Fees_status
     Dim con As New OdbcConnection("DSN=SHREEDSN")
 
-    Private Sub Session_Combo_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles Session_Combo.Click
-        Try
-            con.Open()
-            Dim cmd As OdbcCommand
-            Session_Combo.Items.Clear()
-            cmd = New OdbcCommand("Select Distinct Year from student_Info", con)
-            Dim dr As OdbcDataReader
-            dr = cmd.ExecuteReader
-            While dr.Read
-                Session_Combo.Items.Add(dr.Item(0))
-            End While
-        Catch ex As Exception
-            MsgBox(ex.ToString)
-        End Try
-        con.Close()
-    End Sub
+    'Private Sub Session_Combo_Click(ByVal sender As Object, ByVal e As System.EventArgs)
+    '    Try
+    '        con.Open()
+    '        Dim cmd As OdbcCommand
+    '        Session_Combo.Items.Clear()
+    '        cmd = New OdbcCommand("Select Distinct Year from student_Info", con)
+    '        Dim dr As OdbcDataReader
+    '        dr = cmd.ExecuteReader
+    '        While dr.Read
+    '            Session_Combo.Items.Add(dr.Item(0))
+    '        End While
+    '    Catch ex As Exception
+    '        MsgBox(ex.ToString)
+    '    End Try
+    '    con.Close()
+    'End Sub
 
     Private Sub Class_Combo_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles Class_Combo.Click
         Try
@@ -58,64 +58,64 @@ Public Class Fees_status
 
     End Sub
 
-    Private Sub Subid_Combo_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles Subid_Combo.Click
-        Try
-            con.Open()
-            Dim cmd As OdbcCommand
-            Subid_Combo.Items.Clear()
-            cmd = New OdbcCommand("select Sub_Id from Assign_Subject where Student_Id='" + Studid_Combo.Text + "'", con)
-            Dim dr As OdbcDataReader
-            dr = cmd.ExecuteReader
-            While dr.Read
-                Subid_Combo.Items.Add(dr.Item(0))
-            End While
-        Catch ex As Exception
-            MsgBox(ex.ToString)
-        End Try
-        con.Close()
-    End Sub
+    'Private Sub Subid_Combo_Click(ByVal sender As Object, ByVal e As System.EventArgs)
+    '    Try
+    '        con.Open()
+    '        Dim cmd As OdbcCommand
+    '        Subid_Combo.Items.Clear()
+    '        cmd = New OdbcCommand("select Sub_Id from Assign_Subject where Student_Id='" + Studid_Combo.Text + "'", con)
+    '        Dim dr As OdbcDataReader
+    '        dr = cmd.ExecuteReader
+    '        While dr.Read
+    '            Subid_Combo.Items.Add(dr.Item(0))
+    '        End While
+    '    Catch ex As Exception
+    '        MsgBox(ex.ToString)
+    '    End Try
+    '    con.Close()
+    'End Sub
 
 
-    Private Sub Subid_Combo_TextChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles Subid_Combo.TextChanged
-        Try
-            con.Open()
-            Dim cmd As OdbcCommand
-            cmd = New OdbcCommand("select Fees from Add_Subject where Sub_Id='" + Subid_Combo.Text + "'", con)
-            Dim dr As OdbcDataReader
-            dr = cmd.ExecuteReader
-            While dr.Read
-                amount.Text = dr(0)
-            End While
-        Catch ex As Exception
-            MsgBox(ex.ToString)
-        End Try
-        con.Close()
-    End Sub
+    'Private Sub Subid_Combo_TextChanged(ByVal sender As Object, ByVal e As System.EventArgs)
+    '    Try
+    '        con.Open()
+    '        Dim cmd As OdbcCommand
+    '        cmd = New OdbcCommand("select Fees from Add_Subject where Sub_Id='" + Subid_Combo.Text + "'", con)
+    '        Dim dr As OdbcDataReader
+    '        dr = cmd.ExecuteReader
+    '        While dr.Read
+    '            amount.Text = dr(0)
+    '        End While
+    '    Catch ex As Exception
+    '        MsgBox(ex.ToString)
+    '    End Try
+    '    con.Close()
+    'End Sub
 
     Private Sub paid_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles paid.Click
-        Try
-            con.Open()
-            Dim cmd As OdbcCommand
-            cmd = New OdbcCommand("insert into Fees_Status values('" + Session_Combo.Text + "','" + Class_Combo.Text + "','" + Studid_Combo.Text + "','" + Subid_Combo.Text + "','" + Month_Combo.Text + "','" + checkddno.Text + "','" + amount.Text + "')", con)
-            cmd.ExecuteNonQuery()
-            MsgBox("Fees Paid Successfully")
-            Session_Combo.Text = ""
-            Class_Combo.Text = ""
-            Studid_Combo.Text = ""
-            Subid_Combo.Text = ""
-            Month_Combo.Text = ""
-            amount.Text = ""
-            percentbox.Clear()
-            discountamountbox.Clear()
-            perdiscountbox.Clear()
-            checkddno.Clear()
-            cashradio.Checked = True
-            noradio.Checked = True
-            amountpaidbox.Clear()
-        Catch ex As Exception
-            MsgBox(ex.ToString)
-        End Try
-        con.Close()
+        'Try
+        '    con.Open()
+        '    Dim cmd As OdbcCommand
+        '    cmd = New OdbcCommand("insert into Fees_Status values('" + Session_Combo.Text + "','" + Class_Combo.Text + "','" + Studid_Combo.Text + "','" + Subid_Combo.Text + "','" + Month_Combo.Text + "','" + checkddno.Text + "','" + amount.Text + "')", con)
+        '    cmd.ExecuteNonQuery()
+        '    MsgBox("Fees Paid Successfully")
+        '    Session_Combo.Text = ""
+        '    Class_Combo.Text = ""
+        '    Studid_Combo.Text = ""
+        '    Subid_Combo.Text = ""
+        '    Month_Combo.Text = ""
+        '    amount.Text = ""
+        '    percentbox.Clear()
+        '    discountamountbox.Clear()
+        '    perdiscountbox.Clear()
+        '    checkddno.Clear()
+        '    cashradio.Checked = True
+        '    noradio.Checked = True
+        '    amountpaidbox.Clear()
+        'Catch ex As Exception
+        '    MsgBox(ex.ToString)
+        'End Try
+        'con.Close()
 
     End Sub
 
@@ -223,5 +223,15 @@ Public Class Fees_status
         If ddradio.Checked = True Then
             checkddno.Clear()
         End If
+    End Sub
+
+    Private Sub Subid_Combo_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+
+  
+   
+    Private Sub feesDiscountToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles feesDiscountToolStripMenuItem.Click
+        Fees__Discount.ShowDialog()
     End Sub
 End Class
